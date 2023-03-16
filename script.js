5d54bce11eb333da4ba5712656566a2cba427314
Leonel Stanton <leoneljuanstanton@hotmail.com>
2023-03-16 08:17:21 -0300

    Segundo Commit

--- script.js
+++ script.js
@@ -1,87 +1,166 @@
+class product{
+  constructor (nombre,precio){
+    this.nombre=nombre;
+    this.precio=precio;
+  }
 
-let nomPriProducto= prompt("Ingrese el nombre del primer producto");
-while ((nomPriProducto=="") || (!isNaN(nomPriProducto))){
-    nomPriProducto= prompt("Ingrese el nombre del primer producto");
 }
 
-let prePriProducto= parseFloat(prompt("Ingrese el precio del primer producto"));
-while ((prePriProducto=="") || (prePriProducto==0) || (isNaN(prePriProducto)) ){
-    prePriProducto= parseFloat(prompt("Ingrese el precio del primer producto"));
+function iniciar(){
+const productArray = [];
+let termine=false;
+let indice=1;
+agregarProduc(indice);
+indice++;
+while (!termine){
+  let seguirAgregando = parseInt(prompt(
+      "----------------------------------------------------------------------------" +
+        "\n" +
+        "Quiere seguir agregando productos a su compra:" +
+        "\n" +
+        "(1) Si" +
+        "\n" +
+        "(2) No" +
+        "\n" +
+        "----------------------------------------------------------------------------"
+    ))
+    if (seguirAgregando==1){
+      agregarProduc(indice);
+      indice++;
+    }
+    else termine=true;
 }
 
-let nomSegProducto= prompt("Ingrese el nombre del segundo producto");
-while ((nomSegProducto=="") || (!isNaN(nomSegProducto))){
-    nomSegProducto= prompt("Ingrese el nombre del segundo producto");
+mostrarProductos();
+let opcion =
+  prompt(
+    "----------------------------------------------------------------------------" +
+      "\n" +
+      "Ahora por favor ingrese una de las siguientes opciones:" +
+      "\n" +
+      "- Ingrese el nombre del producto que desea adquirir" +
+      "\n" +
+      "- Ingrese un monto y le mostramos que producto puede comprar" +
+      "\n" +
+      "- No ingrese nada si desea adquirir todos" +
+      "\n" +
+      "----------------------------------------------------------------------------"
+  );
+
+const estaProduc = productArray.some(Producto => Producto.nombre === opcion);
+while (opcion !="" && !estaProduc && isNaN(opcion) ) {
+  opcion = 
+    prompt(
+      "----------------------------------------------------------------------------" +
+        "\n" +
+        "Por favor ingrese una opcion correcta:" +
+        "\n" +
+        "(#) Ingrese el nombre del producto que desea adquirir" +
+        "\n" +
+        "(#) Ingrese un monto y le mostramos que producto puede comprar" +
+        "\n" +
+        "(#) No ingrese nada si desea adquirir todos" +
+        "\n" +
+        "----------------------------------------------------------------------------"
+    );
 }
 
-let preSegProducto=parseFloat(prompt("Ingrese el precio del segundo producto"));
-while ((preSegProducto=="") || (preSegProducto==0) || (isNaN(preSegProducto))){
-    preSegProducto=parseFloat(prompt("Ingrese el precio del segundo producto"));
+if (opcion == "") {
+  let total = productArray.reduce((acumulador,elemento)=> acumulador + elemento.precio,0);
+  alert(
+    "----------------------------------------------------------------------------" +
+      "\n" +
+      "Usted eligio adquirir todos los productos. A continuacion le mostramos como quedaria el monto con los planes de compra que tenemos disponibles :" +
+      "\n" +
+      "• Monto Total: $ " + total +
+      "\n" +
+      "➜ 20% de descuento en efectivo: $ " +
+      calculaefectivo(total) +
+      "\n" +
+      "➜ 30% de descuento con cuenta DNI: $ " +
+      calculacuentadni(total) +
+      "\n" +
+      "➜ 3 cuotas sin interes con cualquier tarjeta: 3x $ " +
+      calculartarjeta3cuotas(total) +
+      "\n" +
+      "➜ 6 cuotas: 6x $ " +
+      calculartarjeta6cuotas(total) +
+      "\n" +
+      "----------------------------------------------------------------------------"
+  );
+} else if (!isNaN(opcion)){
+  const nuevoArray = productArray.filter((productosBuscados) => productosBuscados.precio <= opcion);
+  alert("Usted puede adquirir alguno/s de los siguientes productos: \n");
+  for (let j = 0; j < nuevoArray.length; j++){
+    alert("✔ " + nuevoArray[j].nombre + ": $ " + nuevoArray[j].precio +"\n")
+  };
+}
+else{
+  const buscado = productArray.find(productoBuscado => productoBuscado.nombre == opcion)
+  alert(
+    "----------------------------------------------------------------------------" +
+      "\n" +
+      "Usted eligio adquirir el producto "+buscado.nombre+ ". A continuacion le mostramos como quedaria el monto con los planes de compra que tenemos disponibles :" +
+      "\n" +
+      "• Monto Total: $ " +
+      (buscado.precio) +
+      "\n" +
+      "➜ 20% de descuento en efectivo: $ " +
+      calculaefectivo(buscado.precio) +
+      "\n" +
+      "➜ 30% de descuento con cuenta DNI: $ " +
+      calculacuentadni(buscado.precio) +
+      "\n" +
+      "➜ 3 cuotas sin interes con cualquier tarjeta: 3x $ " +
+      calculartarjeta3cuotas(buscado.precio) +
+      "\n" +
+      "➜ 6 cuotas: 6x $ " +
+      calculartarjeta6cuotas(buscado.precio) +
+      "\n" +
+      "----------------------------------------------------------------------------"
+  );
 }
 
 
-let opcion = parseInt(prompt("----------------------------------------------------------------------------" + "\n" +
-"Usted selecciono los siguientes productos :" + "\n" +
-"✔" + nomPriProducto + ": $ " + prePriProducto + "\n" +
-"✔" + nomSegProducto + ": $ " + preSegProducto + "\n" +
-"\n" + "Ahora por favor elija una de las siguientes opciones :" + "\n" +
-"(1) Comprar solo el primer producto" + "\n" +
-"(2) Comprar solo el segundo producto" + "\n" +
-"(3) Comprar ambos" + "\n"
-+ "----------------------------------------------------------------------------"));
+function agregarProduc(indice){
+  let nomProducto = prompt("Ingrese el nombre del " +indice+ "° producto");
+while (nomProducto == "" || !isNaN(nomProducto)) {
+  nomProducto = prompt("Ingrese el nombre del " +indice+ "° producto");
+}
+let preProducto = parseInt(
+  prompt("Ingrese el precio del " +indice+ "° producto")
+);
+while (preProducto == "" || preProducto == 0 || isNaN(preProducto)) {
+  preProducto = parseInt(prompt("Ingrese el precio del " +indice+ "° producto"));
+}
+const productoNue = new product ( nomProducto,preProducto);
+productArray.push(productoNue);
+}
 
-while ((opcion!=1) && (opcion!=2) && (opcion!=3)){
-    opcion= parseInt(prompt("----------------------------------------------------------------------------" + "\n" +
-    "Por favor ingrese una opcion correcta:" + "\n" +
-    "(1) Comprar solo el primer producto" + "\n" +
-    "(2) Comprar solo el segundo producto" + "\n" +
-    "(3) Comprar ambos" + "\n" + 
-    "----------------------------------------------------------------------------"));
+function mostrarProductos(){
+  for (let i = 0; i < (indice-1); i++){
+  alert ("✔ " + productArray[i].nombre + ": $ " +productArray[i].precio +"\n")
+};
+}
+
+function calculaefectivo(monto) {
+  return monto * 0.8;
+}
+
+function calculacuentadni(monto) {
+  return monto * 0.7;
+}
+
+function calculartarjeta3cuotas(monto) {
+  return monto / 3;
+}
+
+function calculartarjeta6cuotas(monto) {
+  monto = monto + monto * 0.483;
+  return monto / 6;
+}
+
 }
 
 
-function calculaefectivo(monto){
-    return(monto*0.8);
-}
-
-function calculacuentadni(monto){
- return (monto*0.7);
-}
-
-function calculartarjeta3cuotas (monto){
-     return (monto/3);
-}
-
-function calculartarjeta6cuotas (monto){
-    monto= (monto + (monto*0.483));
-    return (monto/6);
-}
-
-if (opcion==1){  
-    alert("----------------------------------------------------------------------------" + "\n" +
-    "Usted eligio adquirir solo el primer producto. A continuacion le mostramos como quedaria el monto con los planes de compra que tenemos disponibles :" + "\n" +
-    "• Monto Total: $ " + prePriProducto + "\n"+
-    "➜ 20% de descuento en efectivo: $ " + calculaefectivo(prePriProducto) + "\n"+
-    "➜ 30% de descuento con cuenta DNI: $ " + calculacuentadni(prePriProducto) + "\n"+
-    "➜ 3 cuotas sin interes con cualquier tarjeta: 3x $ " + calculartarjeta3cuotas(prePriProducto) + "\n"+
-    "➜ 6 cuotas: 6x $ " + calculartarjeta6cuotas(prePriProducto) + "\n" + "----------------------------------------------------------------------------");
-}
-else if (opcion==2){
-    alert("----------------------------------------------------------------------------" + "\n" +
-    "Usted eligio adquirir solo el segundo producto. A continuacion le mostramos como quedaria el monto con los planes de compra que tenemos disponibles :" + "\n" +
-    "• Monto Total: $ " + preSegProducto + "\n"+
-    "➜ 20% de descuento en efectivo: $ " + calculaefectivo(preSegProducto) + "\n"+
-    "➜ 30% de descuento con cuenta DNI: $ " + calculacuentadni(preSegProducto) + "\n"+
-    "➜ 3 cuotas sin interes con cualquier tarjeta: 3x $ " + calculartarjeta3cuotas(preSegProducto) + "\n"+
-    "➜ 6 cuotas: 6x $ " + calculartarjeta6cuotas(preSegProducto) + "\n" + "----------------------------------------------------------------------------");
-}
-else {
-    alert("----------------------------------------------------------------------------" + "\n" +
-    "Usted eligio adquirir ambos productos. A continuacion le mostramos como quedaria el monto con los planes de compra que tenemos disponibles :" + "\n" +
-    "• Monto Total: $ " + (prePriProducto+preSegProducto) + "\n"+
-    "➜ 20% de descuento en efectivo: $ " + calculaefectivo(preSegProducto+prePriProducto) + "\n"+
-    "➜ 30% de descuento con cuenta DNI: $ " + calculacuentadni(preSegProducto+prePriProducto) + "\n"+
-    "➜ 3 cuotas sin interes con cualquier tarjeta: 3x $ " + calculartarjeta3cuotas(preSegProducto+prePriProducto) + "\n"+
-    "➜ 6 cuotas: 6x $ " + calculartarjeta6cuotas(preSegProducto+prePriProducto) + "\n" + "----------------------------------------------------------------------------");
-}
-
+iniciar();
