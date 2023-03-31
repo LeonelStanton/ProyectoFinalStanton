02b566f5ad64961a7950d5aafc6f3cfdd59d9a94
Leonel Stanton <leoneljuanstanton@hotmail.com>
2023-03-30 22:03:48 -0300

    primer commit

--- script.js
+++ script.js
@@ -1,166 +1,186 @@
-class product{
-  constructor (nombre,precio){
-    this.nombre=nombre;
-    this.precio=precio;
-  }
 
+const productos = [
+{
+  id:1,
+nombre:"Buzo de Algodon",
+precio:4898,
+img:"buzoAlgodon.jpg",
+cantidad:0
+},
+{
+  id:2,
+  nombre:"Buzo Bordado",
+  precio:12320,
+  img:"buzoBordado.jpg",
+  cantidad:0
+}, 
+{
+  id:3,
+  nombre:"Buzos Frizados",
+  precio:7560,
+  img:"buzoFrizado.jpg",
+  cantidad:0
+},
+{
+  id:4,
+  nombre:"Jean",
+  precio:8680,
+  img:"jean.jpg",
+  cantidad:0
+},
+{
+  id:5,
+  nombre:"Jogger",
+  precio:10080,
+  img:"jogger.jpg",
+  cantidad:0
+},
+{
+  id:6,
+  nombre:"Jogger con Rotura",
+  precio:10080,
+  img:"joggerCRotura.jpg",
+  cantidad:0
+},
+{
+  id:7,
+  nombre:"Remera Glorius",
+  precio:4980,
+  img:"remeraGlorius.jpg",
+  cantidad:0
+},
+{
+  id:8,
+  nombre:"Remera Break",
+  precio:4980,
+  img:"remeraBreak.jpg",
+  cantidad:0
+},
+{
+  id:9,
+  nombre:"Remera Chill",
+  precio:4980,
+  img:"remeraChill.jpg",
+  cantidad:0
 }
+];
 
-function iniciar(){
-const productArray = [];
-let termine=false;
-let indice=1;
-agregarProduc(indice);
-indice++;
-while (!termine){
-  let seguirAgregando = parseInt(prompt(
-      "----------------------------------------------------------------------------" +
-        "\n" +
-        "Quiere seguir agregando productos a su compra:" +
-        "\n" +
-        "(1) Si" +
-        "\n" +
-        "(2) No" +
-        "\n" +
-        "----------------------------------------------------------------------------"
-    ))
-    if (seguirAgregando==1){
-      agregarProduc(indice);
-      indice++;
+let carrito = []
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        renderCarrito()
     }
-    else termine=true;
+})
+const ventanaSecundaria = document.getElementById("ventanaSecundaria")
+const main = document.querySelector(".ventanaPrincipal")
+
+
+function renderProductos(){
+  
+      productos.forEach((prod)=>{
+       main.innerHTML += `<div class="card">
+       <div class="containFoto">
+       <img src=${"img/" + prod.img} class="fotos" alt= "">
+       </div>
+       <div class="containProduct">
+       <h3 class="nombreProducto">${prod.nombre}</h3>
+       <p class="precioProducto">Precio: $${prod.precio}</p>
+       <p class="precio3cuotas">3 cuotas sin interés de $${calculartarjeta3cuotas(prod.precio)}</p>
+       <button id="agregar${prod.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
+       </div>
+       </div>`
+})
+activarBotones();
 }
 
-mostrarProductos();
-let opcion =
-  prompt(
-    "----------------------------------------------------------------------------" +
-      "\n" +
-      "Ahora por favor ingrese una de las siguientes opciones:" +
-      "\n" +
-      "- Ingrese el nombre del producto que desea adquirir" +
-      "\n" +
-      "- Ingrese un monto y le mostramos que producto puede comprar" +
-      "\n" +
-      "- No ingrese nada si desea adquirir todos" +
-      "\n" +
-      "----------------------------------------------------------------------------"
-  );
-
-const estaProduc = productArray.some(Producto => Producto.nombre === opcion);
-while (opcion !="" && !estaProduc && isNaN(opcion) ) {
-  opcion = 
-    prompt(
-      "----------------------------------------------------------------------------" +
-        "\n" +
-        "Por favor ingrese una opcion correcta:" +
-        "\n" +
-        "(#) Ingrese el nombre del producto que desea adquirir" +
-        "\n" +
-        "(#) Ingrese un monto y le mostramos que producto puede comprar" +
-        "\n" +
-        "(#) No ingrese nada si desea adquirir todos" +
-        "\n" +
-        "----------------------------------------------------------------------------"
-    );
-}
-
-if (opcion == "") {
-  let total = productArray.reduce((acumulador,elemento)=> acumulador + elemento.precio,0);
-  alert(
-    "----------------------------------------------------------------------------" +
-      "\n" +
-      "Usted eligio adquirir todos los productos. A continuacion le mostramos como quedaria el monto con los planes de compra que tenemos disponibles :" +
-      "\n" +
-      "• Monto Total: $ " + total +
-      "\n" +
-      "➜ 20% de descuento en efectivo: $ " +
-      calculaefectivo(total) +
-      "\n" +
-      "➜ 30% de descuento con cuenta DNI: $ " +
-      calculacuentadni(total) +
-      "\n" +
-      "➜ 3 cuotas sin interes con cualquier tarjeta: 3x $ " +
-      calculartarjeta3cuotas(total) +
-      "\n" +
-      "➜ 6 cuotas: 6x $ " +
-      calculartarjeta6cuotas(total) +
-      "\n" +
-      "----------------------------------------------------------------------------"
-  );
-} else if (!isNaN(opcion)){
-  const nuevoArray = productArray.filter((productosBuscados) => productosBuscados.precio <= opcion);
-  alert("Usted puede adquirir alguno/s de los siguientes productos: \n");
-  for (let j = 0; j < nuevoArray.length; j++){
-    alert("✔ " + nuevoArray[j].nombre + ": $ " + nuevoArray[j].precio +"\n")
-  };
-}
-else{
-  const buscado = productArray.find(productoBuscado => productoBuscado.nombre == opcion)
-  alert(
-    "----------------------------------------------------------------------------" +
-      "\n" +
-      "Usted eligio adquirir el producto "+buscado.nombre+ ". A continuacion le mostramos como quedaria el monto con los planes de compra que tenemos disponibles :" +
-      "\n" +
-      "• Monto Total: $ " +
-      (buscado.precio) +
-      "\n" +
-      "➜ 20% de descuento en efectivo: $ " +
-      calculaefectivo(buscado.precio) +
-      "\n" +
-      "➜ 30% de descuento con cuenta DNI: $ " +
-      calculacuentadni(buscado.precio) +
-      "\n" +
-      "➜ 3 cuotas sin interes con cualquier tarjeta: 3x $ " +
-      calculartarjeta3cuotas(buscado.precio) +
-      "\n" +
-      "➜ 6 cuotas: 6x $ " +
-      calculartarjeta6cuotas(buscado.precio) +
-      "\n" +
-      "----------------------------------------------------------------------------"
-  );
+function activarBotones(){
+  productos.forEach((producto)=>{
+    document.getElementById(`agregar${producto.id}`).addEventListener(`click`,()=>{agregarAlCarrito(producto)})
+  })
 }
 
 
-function agregarProduc(indice){
-  let nomProducto = prompt("Ingrese el nombre del " +indice+ "° producto");
-while (nomProducto == "" || !isNaN(nomProducto)) {
-  nomProducto = prompt("Ingrese el nombre del " +indice+ "° producto");
-}
-let preProducto = parseInt(
-  prompt("Ingrese el precio del " +indice+ "° producto")
-);
-while (preProducto == "" || preProducto == 0 || isNaN(preProducto)) {
-  preProducto = parseInt(prompt("Ingrese el precio del " +indice+ "° producto"));
-}
-const productoNue = new product ( nomProducto,preProducto);
-productArray.push(productoNue);
+function agregarAlCarrito(prod) {
+  let existe =carrito.some(item => item.id === prod.id);
+  if (existe) {
+      const miProd = carrito.find(item => item.id === prod.id);
+      miProd.cantidad += 1;
+  } else {
+      prod.cantidad = 1;
+      carrito.push(prod);
+  }
+  renderCarrito();
+  renderBotonCarrito();
 }
 
-function mostrarProductos(){
-  for (let i = 0; i < (indice-1); i++){
-  alert ("✔ " + productArray[i].nombre + ": $ " +productArray[i].precio +"\n")
-};
+
+function renderBotonCarrito(){
+document.getElementById("contadorCarrito").innerText =carrito.reduce((total, item) => total += item.cantidad, 0);
+
+}
+
+function renderCarrito(){
+  ventanaSecundaria.innerHTML = "" 
+    carrito.forEach((prod) => {
+        const div = document.createElement('div')
+        div.className = ('productoEnCarrito')
+        div.innerHTML = `
+        <p>${prod.nombre}</p>
+        <p>Precio:$${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
+        ventanaSecundaria.appendChild(div)
+        localStorage.setItem(`carrito`,JSON.stringify(carrito))
+  
+       
+})
+renderBotonCarrito()
+preciototal()
+}
+
+function preciototal(){
+  const precioTotal = document.getElementById('precioTotal')
+  const precioCDescuento = document.getElementById('precioefectivo')
+  const precioECuotas = document.getElementById('precio3cuotas')
+  let montototal = parseInt(carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0));
+  precioTotal.innerText = montototal
+  precioCDescuento.innerText = calculaefectivo(montototal)
+  precioECuotas.innerText = calculartarjeta3cuotas(montototal)
+  
 }
 
 function calculaefectivo(monto) {
-  return monto * 0.8;
-}
-
-function calculacuentadni(monto) {
-  return monto * 0.7;
+  return Math.round(monto * 0.75);
 }
 
 function calculartarjeta3cuotas(monto) {
-  return monto / 3;
+  return Math.round(monto / 3);
 }
 
-function calculartarjeta6cuotas(monto) {
-  monto = monto + monto * 0.483;
-  return monto / 6;
+function vaciarCarrito(){
+  carrito.length = 0;
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  renderBotonCarrito()
+  preciototal()  
+  renderCarrito()
 }
 
+function eliminarDelCarrito (prodId) {
+  const item = carrito.find((prod) => prod.id === prodId)
+  const indice = carrito.indexOf(item)
+  carrito.splice(indice, 1) 
+  localStorage.setItem("carrito", JSON.stringify(carrito))
+  renderBotonCarrito()
+  preciototal()  
+  renderCarrito()
 }
 
 
-iniciar();
+renderProductos()
+
+
+
+
+
